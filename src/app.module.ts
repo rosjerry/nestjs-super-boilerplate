3927b0ed4ba3e35cb84ec0a02188cd9a33c141a6
rosjerry <rostomi1306@gmail.com>
2025-08-14 17:57:59 +0400

    feat: implement gql into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloRestModule } from './hello-rest/hello-rest.module';
 import { HelloGqlCfModule } from './hello-gql-cf/hello-gql-cf.module';
 
 @Module({
-  imports: [HelloRestModule, HelloGqlCfModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+    HelloRestModule,
+    HelloGqlCfModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
