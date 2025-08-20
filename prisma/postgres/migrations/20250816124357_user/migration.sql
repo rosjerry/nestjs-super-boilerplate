49cdcb955f5a5e3d7ce80263800cd938791c24c3
rosjerry <rostomi1306@gmail.com>
2025-08-20 16:17:57 +0400

    feat: change postgresql migration

--- prisma/postgres/migrations/20250816124357_user/migration.sql
+++ prisma/postgres/migrations/20250816124357_user/migration.sql
@@ -1,11 +1,9 @@
 -- CreateTable
 CREATE TABLE "public"."users" (
-    "pk" SERIAL NOT NULL,
     "id" UUID NOT NULL,
     "email" TEXT NOT NULL,
-    "phone_number" TEXT,
-    "password_hash" TEXT,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("pk")
 );
@@ -15,9 +13,3 @@ CREATE UNIQUE INDEX "users_id_key" ON "public"."users"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_phone_number_key" ON "public"."users"("phone_number");
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_pk_id_key" ON "public"."users"("pk", "id");
