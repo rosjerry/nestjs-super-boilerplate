9ead233f038ca724a75441deecec588b75d146e1
rosjerry <rostomi1306@gmail.com>
2025-08-20 17:03:49 +0400

    chore: delete unused module

--- src/gql-cf/dto/update-gql-cf.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateGqlCfInput } from './create-gql-cf.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateGqlCfInput extends PartialType(CreateGqlCfInput) {
-  @Field(() => Int)
-  id: number;
-}
